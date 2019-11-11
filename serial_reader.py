b8542c56c1057b8a844a8233484dc34967f4aba9
Alex Serebriiskii <as4377@drexel.edu>
2019-11-11 16:42:25 -0500

    changed code to identify serial port

--- serial_reader.py
+++ serial_reader.py
@@ -2,10 +2,11 @@
 import serial, string
 
 output = " "
-ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
-while True:
-  print "----"
-  while output != "":
-    output = ser.readline()
-    print output
-  output = " "
+ser = serial.Serial('/dev/ttyUSB0')
+print(ser.name)
+# while True:
+#   print "----"
+#   while output != "":
+#     output = ser.readline()
+#     print (output)
+#   output = " "
