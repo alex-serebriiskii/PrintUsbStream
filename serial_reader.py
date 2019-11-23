10a64b23207fa745e1e999b3f85d3831f3845667
AlexSerebriiskii <as4377@drexel.edu>
2019-11-22 22:14:05 -0500

    Added printing command to listning loop

--- serial_reader.py
+++ serial_reader.py
@@ -1,26 +1,51 @@
 #!/usr/bin/python3
 #Based on https://www.raspberrypi.org/forums/viewtopic.php?t=106468
 
-import serial, string
+import serial, string, time, os
 
 path = '/home/alex/test.txt'
-
+ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1)
+ser.timeout = 10
 def main():
-  file = open(path, 'w')
-  if(filewriter(file)):
-    file.close
+  if(filewriter()):
+    exit
 
-def filewriter(file):
-  ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1)
+def filewriter():
+  
+  file = open(path, 'wb')
+  contenttoprint = False;
   print(ser.name) 
   output = " "
   while True:
     print ("####New Transmission####\n")
     while output != "":
       output = ser.readline()
-      file.write(output.decode('UTF8'))
       print (output)
+      file.write(output)
+      if(output == b''):
+        print("Output is empty")
+        if contenttoprint == True:
+          print("printing")
+          file.close()
+          os.system("lpr /home/alex/test.txt")
+          #print something here
+          file = open(path, 'wb')
+        contenttoprint = False
+      else:
+        contenttoprint = True
+
     print("#### End of Transmission####\n")
     return True
     output = " "
+
+def timedfilewriter(file, timeout):
+  print(ser.name)
+  print("Start Reading\n")
+  tic = time.time()
+  while (time.time() - tic) < timeout:
+    output = ser.readline()
+    file.write(output.decode('UTF8'))
+    print(output)
+    tic = time.time
+  return True
 main()
