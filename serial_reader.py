5a262580eb1b97cec1ef474821302e9f6f3e5ba9
AlexSerebriiskii <as4377@drexel.edu>
2019-11-29 15:36:37 -0500

    changed file output creation to use current user instead of hard coded one

--- serial_reader.py
+++ serial_reader.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 #Based on https://www.raspberrypi.org/forums/viewtopic.php?t=106468
 
-import serial, string, time, os
+import serial, string, time, os, getpass
 
-path = '/home/alex/test.txt'
+user = getpass.getuser()
+path = '/home/%s/test.txt' %(user)
 ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1)
 ser.timeout = 10
 def main():
