cbffa9601f6d722053cb6ea11384f772ed7c2959
alex <as4377@drexel.edu>
2020-11-18 16:13:03 -0500

    Started work on having script start on boot

--- env_setup.sh
+++ env_setup.sh
@@ -41,6 +41,35 @@ else
     sudo apt install chromium-browser
 fi
 
+echo "Converting Script to Service"
+
+if [ -f "/etc/systemd/system/USB_Radio.service"]; then
+    #remove version of the service
+    sudo rm /etc/systemd/system/USB_Radio.service
+    #TODO find and kill previous version of service if there is one
+fi
+#Start file creation
+touch ./USB_Radio.service
+
+echo "[Unit]" >> USB_Radio.service
+echo "Description=A description for your custom service goes here " >> USB_Radio.service
+echo "After=network.target" >> USB_Radio.service
+
+echo "[Service]" >> USB_Radio.service
+echo "Type=simple" >> USB_Radio.service
+echo "ExecStart=/usr/bin/python3 /home/$USER/RadioSerialPrinter/PrintUsbStream/serial_reader.py" >> USB_Radio.service
+echo "TimeoutStartSec=0" >> USB_Radio.service
+
+echo "[Install]" >> USB_Radio.service
+echo "WantedBy=default.target" >> USB_Radio.service
+#end file creation 
+
+echo "Setting up service to run on reboot"
+#sudo mv ./USB_Radio.service /etc/system/systemd
+#sudo systemctl enable USB_Radio.service
+#TODO check if service was created properly
+#systemctl start USB_Radio.service
+
 echo "Please set up the printer.."
 chromium-browser http://localhost:631/admin &
 
--- guitest.py
+++
@@ -1,4 +0,0 @@
-from guizero import App, Text
-app = App(title="Hello world")
-message = Text(app, text="Welcome to the Hello world app!")
-app.display()
